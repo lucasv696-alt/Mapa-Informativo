3eebb707af6c59f22b497a4c3c40c1cbf566e640
Lucas Jose Vazquez <lucasv696@gmail.com>
2026-07-05 18:59:42 -0300

    Version 0.4 - Carga de lugares desde JSON

--- js/app.js
+++ js/app.js
@@ -1,10 +1,26 @@
-const map = L.map('map').setView([-34.6037, -58.3816], 12);
+const map = L.map('map').setView([-34.6037, -58.3816], 13);
 
 L.tileLayer('https://tile.openstreetmap.org/{z}/{x}/{y}.png', {
     attribution: '&copy; OpenStreetMap'
 }).addTo(map);
 
-L.marker([-34.6037, -58.3816])
-    .addTo(map)
-    .bindPopup('¡Primer marcador!')
-    .openPopup();
+fetch('data/lugares.json')
+    .then(response => response.json())
+    .then(lugares => {
+
+        lugares.forEach(lugar => {
+
+            L.marker([lugar.latitud, lugar.longitud])
+                .addTo(map)
+                .bindPopup(`
+                    <strong>${lugar.nombre}</strong><br>
+                    ${lugar.direccion}<br>
+                    ${lugar.descripcion}
+                `);
+
+        });
+
+    })
+    .catch(error => {
+        console.error("Error al cargar los lugares:", error);
+    });
